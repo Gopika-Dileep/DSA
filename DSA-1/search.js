ea81ea30d26ec1ddea1d84423963e6e19f29b078
Gopika Dileep <gopikadileep76@gmail.com>
2025-04-02 19:03:42 +0530

    binarysearch

--- DSA-1/search.js
+++ DSA-1/search.js
@@ -17,3 +17,21 @@
 // -------------------------------------binarysearch------------------------------------------------------
 
 
+// function binary(arr,target){
+//     let left=0
+//     let right=arr.length-1
+//     while(left<=right){
+//         let middle=Math.floor((left+right)/2)
+//         if(arr[middle]===target){
+//             return middle
+//         }
+//         if(target<arr[middle]){
+//             right=middle-1
+//         }else{
+//             left=middle+1
+//         }
+//     }
+//     return -1
+// }
+
+// console.log(binary([1,2,3,4,5,6,7],3))
