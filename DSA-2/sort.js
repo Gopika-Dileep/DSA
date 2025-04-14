97e4717cb3811b4ff7c3392abee06da1355cf0fc
Gopika Dileep <gopikadileep76@gmail.com>
2025-04-14 13:59:37 +0530

    bubble sort

---
+++ DSA-2/sort.js
@@ -0,0 +1,20 @@
+// ----------------------------------------------bubble sort--------------------------------------------------
+
+// function bubble(arr){
+//     for(i=0;i<arr.length-1;i++){
+//         let flag =0
+//         for(j=0;j<arr.length-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//               let  temp = arr[j]
+//                 arr[j]=arr[j+1]
+//                 arr[j+1]=temp
+//                 flag=1
+//             }
+//         }
+//         if(flag==0){
+//             break
+//         }
+//     }
+//     return arr
+// }
+// console.log(bubble([4,6,7,4,2,1,9]))
