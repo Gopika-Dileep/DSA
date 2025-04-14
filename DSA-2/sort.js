15d1e9f290af0e7704b33d3d41a13039c20983eb
Gopika Dileep <gopikadileep76@gmail.com>
2025-04-14 14:06:31 +0530

    insertion sort

--- DSA-2/sort.js
+++ DSA-2/sort.js
@@ -18,3 +18,21 @@
 //     return arr
 // }
 // console.log(bubble([4,6,7,4,2,1,9]))
+
+
+
+// ----------------------------insertion sort-------------------------------------------------------------------
+
+// function insertion(arr){
+//     for(i=1;i<arr.length;i++){
+//         let temp = arr[i]
+//         let j = i-1
+//         while(j>=0&&arr[j]>temp){
+//             arr[j+1]=arr[j]
+//             j--
+//         }
+//         arr[j+1]=temp
+//     }
+//     return arr
+// }
+// console.log(insertion([5,1,3,2,7,6,3,5,8,9,4]))
